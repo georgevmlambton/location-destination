f40478c140d529b9a673d86e916aa202d75b184a
George Varghese M <georgevm.fsdm@proton.me>
2024-10-15 21:34:26 -0400

    Add profile editing page

---
+++ apps/web/src/pages/profile/Profile.tsx
@@ -0,0 +1,172 @@
+import { useContext, useEffect, useMemo, useState } from 'react';
+import arrowLeft from '../../assets/arrow-left.svg';
+import boxArrowRight from '../../assets/box-arrow-right.svg';
+import personFill from '../../assets/person-fill.svg';
+import check2 from '../../assets/check2.svg';
+import x from '../../assets/x.svg';
+import { Form, Formik } from 'formik';
+import * as yup from 'yup';
+import { getInstance } from '../../axios';
+import {
+  ProfileResponse,
+  ProfilePatchRequest,
+} from '@location-destination/types/src/requests/profile';
+import { Toast, ToastContainer } from 'react-bootstrap';
+import { ToastContext } from '../../providers/toast-provider';
+import { TextField } from '../../components/form/TextField';
+import { NavButton } from '../../components/nav/NavButton';
+import { ProfileField } from './ProfileField';
+
+const validationSchema = yup.object({
+  name: yup.string().required('Name is required'),
+});
+
+function isProfileSetupDone(profile: ProfileResponse) {
+  return !!profile.name;
+}
+
+function isRequiredFieldsFilled(values: { name: string | undefined }) {
+  return !!values.name;
+}
+
+export function Profile() {
+  const [profile, setProfile] = useState<ProfileResponse | null>(null);
+  const canExit = useMemo(
+    () => profile && isProfileSetupDone(profile),
+    [profile]
+  );
+
+  const toast = useContext(ToastContext);
+
+  async function submit(values: { name: string | undefined }) {
+    const axios = await getInstance();
+    const req: ProfilePatchRequest = {
+      name: values.name,
+    };
+
+    try {
+      const response = await axios.patch<ProfileResponse>('/api/profile', req);
+      setProfile(response.data);
+    } catch (e) {
+      toast.show(e.message, 'danger');
+    }
+  }
+
+  useEffect(() => {
+    getInstance()
+      .then(async (axios) => {
+        const response = await axios.get<ProfileResponse>('/api/profile');
+        setProfile(response.data);
+      })
+      .catch((e) => toast.show(e.message, 'danger'));
+  }, [toast]);
+
+  return (
+    <div className="w-100 h-100" style={{ background: '#F8F8F8' }}>
+      <div
+        className="p-4 pb-5"
+        style={{
+          backgroundColor: '#00634B',
+        }}
+      >
+        <div className="d-flex justify-content-between">
+          <NavButton icon={arrowLeft} hidden={!canExit} />
+          <NavButton icon={boxArrowRight} />
+        </div>
+
+        <div className="d-flex align-items-end mt-5">
+          <img
+            className="rounded-circle"
+            src={personFill}
+            style={{
+              width: '100px',
+              height: '100px',
+              border: '2px solid #CCCCCC',
+              borderRadius: '50%',
+              background: 'white',
+            }}
+          />
+          <h2
+            className="ms-3 pb-3 placeholder-glow flex-grow-1"
+            style={{ color: profile?.name ? 'white' : 'rgb(189, 189, 189)' }}
+          >
+            {!profile && (
+              <span className="placeholder" style={{ width: '100%' }}></span>
+            )}
+            {profile && (profile.name || '(Your Name)')}
+          </h2>
+        </div>
+      </div>
+
+      {profile && (
+        <Formik
+          initialValues={{ name: profile.name }}
+          validationSchema={validationSchema}
+          onSubmit={async (values, { setSubmitting, resetForm }) => {
+            setSubmitting(true);
+            await submit(values);
+            setSubmitting(false);
+            resetForm({ values });
+          }}
+        >
+          {({ isSubmitting, errors, touched, dirty, resetForm, values }) => (
+            <Form className="px-4 mt-5">
+              <ProfileField
+                errors={errors}
+                touched={touched}
+                name="name"
+                label="Name"
+                required
+              >
+                <TextField touched={touched} errors={errors} name="name" />
+              </ProfileField>
+
+              {dirty && isRequiredFieldsFilled(values) && (
+                <div className="d-flex position-absolute bottom-0 end-0 p-4">
+                  <button
+                    disabled={isSubmitting}
+                    onClick={() => resetForm()}
+                    className="btn btn-light d-flex justify-content-center align-content-center p-2 me-3"
+                    style={{
+                      border: '1px solid #CCCCCC',
+                      borderRadius: '50%',
+                      boxShadow: 'rgba(0, 0, 0, 0.4) 0px 6px 16px',
+                      width: '52px',
+                      height: '52px',
+                    }}
+                  >
+                    <img src={x} />
+                  </button>
+                  <button
+                    disabled={isSubmitting}
+                    type="submit"
+                    className="btn btn-light d-flex justify-content-center align-content-center p-2"
+                    style={{
+                      border: '1px solid #CCCCCC',
+                      borderRadius: '50%',
+                      boxShadow: 'rgba(0, 0, 0, 0.4) 0px 6px 16px',
+                      width: '52px',
+                      height: '52px',
+                    }}
+                  >
+                    <img src={check2} />
+                  </button>
+                </div>
+              )}
+
+              {!isRequiredFieldsFilled(values) && (
+                <ToastContainer position="bottom-end" className="p-2">
+                  <Toast animation bg="danger">
+                    <Toast.Body className="text-white">
+                      Please fill all required fields
+                    </Toast.Body>
+                  </Toast>
+                </ToastContainer>
+              )}
+            </Form>
+          )}
+        </Formik>
+      )}
+    </div>
+  );
+}
---
+++ apps/web/src/pages/profile/ProfileField.tsx
@@ -0,0 +1,30 @@
+import { FormikErrors, FormikTouched } from 'formik';
+import { ReactNode } from 'react';
+
+export function ProfileField<T>({
+  name,
+  label,
+  required = false,
+  errors,
+  touched,
+  children,
+}: {
+  name: keyof T;
+  label: string;
+  required?: boolean;
+  touched: FormikTouched<T>;
+  errors: FormikErrors<T>;
+  children?: ReactNode;
+}) {
+  return (
+    <div
+      className={`border px-3 py-4 bg-white ${touched[name] && errors[name] ? 'border-danger' : 'border-secondary-subtle'}`}
+      style={{ borderRadius: '25px' }}
+    >
+      <label className="mb-4">
+        {label} {required && <span className="text-danger">*</span>}
+      </label>
+      {children}
+    </div>
+  );
+}
