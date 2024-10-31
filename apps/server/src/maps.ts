8629508ac31482944d82f03a8eee88fe37c41b83
Ashu Sharma <10071999ashusharma@gmail.com>
2024-10-30 23:06:17 -0400

    Mapbox redis cache

--- apps/server/src/maps.ts
+++ apps/server/src/maps.ts
@@ -1,6 +1,16 @@
 import * as config from './config';
+import { redis } from './redis';
 
 export async function geocodeAddress(address: string) {
+  const cachedLocation = await redis.get(`geocode:${address}`);
+
+  if (cachedLocation) {
+    return JSON.parse(cachedLocation) as {
+      lat: number;
+      lng: number;
+    };
+  }
+
   const geocodeUrl = `https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(address)}&access_token=${config.mapboxToken}`;
   const response = await fetch(geocodeUrl);
   const body = await response.json();
@@ -9,16 +19,30 @@ export async function geocodeAddress(address: string) {
     return null;
   }
 
-  return {
+  const location = {
     lat: body.features[0].geometry.coordinates[1],
     lng: body.features[0].geometry.coordinates[0],
   };
+
+  await redis.set(`geocode:${address}`, JSON.stringify(location), {
+    EX: 30 * 24 * 60 * 60,
+  });
+
+  return location;
 }
 
 export async function getDrivingDurationMinutes(
   p1: { lat: number; lng: number },
   p2: { lat: number; lng: number }
 ) {
+  const cachedDuration = await redis.get(
+    `drivingDuration:${p1.lat},${p1.lng}-${p2.lat},${p2.lng}`
+  );
+
+  if (cachedDuration) {
+    return Number(cachedDuration);
+  }
+
   const coordinates = `${p1.lng},${p1.lat};${p2.lng},${p2.lat}`;
   const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${encodeURIComponent(coordinates)}?alternatives=false&geometries=geojson&language=en&overview=simplified&steps=false&notifications=none&access_token=${config.mapboxToken}`;
   try {
@@ -30,7 +54,13 @@ export async function getDrivingDurationMinutes(
     }
 
     const seconds = body.routes[0].duration;
-    return seconds / 60;
+    const minutes = seconds / 60;
+
+    await redis.set(
+      `drivingDuration:${p1.lat},${p1.lng}-${p2.lat},${p2.lng}`,
+      minutes.toString(),
+      { EX: 2 * 60 * 60 }
+    );
   } catch (_) {
     return null;
   }
